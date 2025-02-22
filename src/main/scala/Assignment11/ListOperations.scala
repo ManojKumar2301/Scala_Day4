7fdc3528c2e488d8b0e092913f29c681d97aae13
ManojKumar2301 <manojkumarpendekanti23@gmail.com>
2025-02-23 00:01:31 +0530

    Scala_Day4

--- src/main/scala/Assignment11/ListOperations.scala
+++ src/main/scala/Assignment11/ListOperations.scala
@@ -1,24 +1,11 @@
 package Assignment11
 
-object ListOperations extends App{
-    val numbers=List(12, 20, 33, 42, 5)
+object ListOperations extends App {
+    val numbers = List(12, 20, 33, 42, 5)
 
-    var doubleNumbers=List[Int]()
-    for(i <- numbers){
-      doubleNumbers=doubleNumbers :+ (i *2)
-    }
-
-    var filterNumbers=List[Int]()
-    for(i <- numbers){
-      if(i%2==0){
-        filterNumbers=filterNumbers :+ i
-      }
-    }
-
-    var sumNumbers=0
-    for(i <- numbers){
-      sumNumbers=sumNumbers+i
-    }
+    val doubleNumbers = numbers.map(_ * 2)
+    val filterNumbers = numbers.filter(_ % 2 == 0)
+    val sumNumbers = numbers.sum
 
     println(s"numbers: $numbers")
     println(s"doubleNumbers: $doubleNumbers")
